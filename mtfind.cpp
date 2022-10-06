b89f01db984d123325bd2aab3fee5da45bd211a0
Alexander Abrosov <aabrosov@gmail.com>
2022-10-06 17:24:27 +0300

    code fixed to process big files

--- mtfind.cpp
+++ mtfind.cpp
@@ -10,53 +10,46 @@ static const size_t BLOCK = 65536;
 
 // Ulysses.txt ~ 23,6 blocks
 
-void d_count(const int n, char* data, size_t length, int results[]) {
+void d_count(const int n, char* data, char* eod, int results[], int lines[]) {
+    int line = 1;
     int counter = 0;
     int position = 0;
-
-    // skip first line if it's not the first part
-    if (n != 0) {
-        while (data && *data != '\n') {
-            ++data;
-            ++position;
-        }
-        ++data;
-        ++position;
-    }
-
-    // process main part
-    while (data && position < length) {
+    // char ch = (*data == '\n') ? '?' : *data;
+    // std::cout << ch << "^" << n << "|" << line << "|" << counter << "|" << position << std::endl;
+    while (data && data < eod) {
         if (*data == 'd') {
             ++counter;
         }
-        ++data;
-        ++position;
-    }
-
-    // process next line if it's not the last part
-    if (n != THREADS - 1) {
-        while (data && *data != '\n') {
-            if (*data == 'd') {
-                ++counter;
-            }
+        if (*data == '\n') {
+            ++line;
             ++data;
             ++position;
+            // ch = (*data == '\n') ? '?' : *data;
+            // std::cout << ch << "-" << n << "|" << line << "|" << counter << "|" << position << std::endl;
+        }
+        if (!data || data == eod) {
+            break;
         }
         ++data;
         ++position;
+        // ch = (*data == '\n') ? '?' : *data;
+        // std::cout << ch << "_" << n << "|" << line << "|" << counter << "|" << position << std::endl;
     }
 
+    lines[n] = line;
     results[n] = counter;
 }
 
 int main() {
     std::vector<std::thread> threads;
 
-    int results[THREADS] = {0};
+    int results[THREADS] = { 0 };
+    int lines[THREADS] = { 0 };
 
     boost::iostreams::mapped_file file;
 
     std::string file_name = "Ulysses.txt";
+    // std::string file_name = "example.txt";
 
     file.open(file_name, boost::iostreams::mapped_file::mapmode::readwrite);
 
@@ -66,21 +59,55 @@ int main() {
 
     if (file.is_open()) {
         char* data = (char*) file.const_data();
-
-        for (int i = 0; i < THREADS; i++) {
-            if (i == THREADS - 1) {
-                length = file_size - BLOCK * (THREADS - 1);
+        char* bod = data;
+        char* eod = data + file_size - 1;
+        int i = 0;
+        size_t position = 0;
+        length = std::min(BLOCK, file_size - position);
+        char* bob = bod;
+        char* eob = bob + length - 1;
+        while (bob && eob) {
+            // shift eob to next eol
+            while (eob && *eob != '\n') {
+                ++eob;
             }
-            threads.push_back(std::thread(d_count, i, data + i * BLOCK, length, results));
+            if (!eob) {
+                break;
+            }
+            // threads.push_back(std::thread(d_count, i, bob, eob, results, lines));
+            d_count(i, bob, eob, results, lines);
+            position += eob - bob + 1;
+            // std::cout << "processed up to position = " << position << std::endl;
+            if (position >= file_size) {
+                break;
+            }
+            bob = eob + 1;
+            if (!bob) {
+                break;
+            }
+            length = std::min(BLOCK, file_size - position);
+            eob = bob + length - 1;
+            if (!eob) {
+                break;
+            }
+            ++i;
         }
 
         for (auto& th : threads) {
             th.join();
         }
 
+        int total_d = 0;
+        int total_lines = 0;
         for (int i = 0; i < THREADS; ++i) {
-            std::cout << "Thread: " << i << ", Result: " << results[i] << std::endl;
+            std::cout << "Thread: " << i;
+            std::cout << ", Result: " << results[i];
+            std::cout << ", Lines: " << lines[i] << std::endl;
+            total_d += results[i];
+            total_lines += lines[i];
         }
+        std::cout << "Total 'd': " << total_d << std::endl;
+        std::cout << "Total lines: " << total_lines << std::endl;
 
         file.close();
     }
