d5e6d554059a389460629a5f2b532081e6ca2791
Alexander Abrosov <aabrosov@gmail.com>
2022-10-07 00:14:30 +0300

    returning results from threads changed

--- mtfind.cpp
+++ mtfind.cpp
@@ -5,12 +5,14 @@
 #include <thread>
 #include <vector>
 
-static const size_t THREADS = 32;
-static const size_t BLOCK = 64;
+static const size_t BLOCK_SIZE = 65536;
 
 // Ulysses.txt ~ 23,6 blocks
 
-void d_count(const int n, char* bob, char* eob, int results[], int lines[], int bytes[]) {
+void d_count(const int n, char* bob, char* eob,
+    std::vector<int> & results,
+    std::vector<int> & lines,
+    std::vector<int> & bytes) {
     int line = 1;
     int counter = 0;
     int by = 1;
@@ -32,21 +34,21 @@ void d_count(const int n, char* bob, char* eob, int results[], int lines[], int 
 int main() {
     std::vector<std::thread> threads;
 
-    int results[THREADS] = { 0 };
-    int lines[THREADS] = { 0 };
-    int bytes[THREADS] = { 0 };
 
     boost::iostreams::mapped_file file;
 
-    // std::string file_name = "Ulysses.txt";
+    std::string file_name = "Ulysses.txt";
     // std::string file_name = "example.txt";
-    std::string file_name = "dabadee.txt";
+    // std::string file_name = "dabadee.txt";
 
     file.open(file_name, boost::iostreams::mapped_file::mapmode::readwrite);
 
     size_t file_size = file.size();
-
-    size_t length = BLOCK;
+    size_t max_blocks = 1 + file_size / BLOCK_SIZE;
+    std::vector<int> results(max_blocks, 0);
+    std::vector<int> lines(max_blocks, 0);
+    std::vector<int> bytes(max_blocks, 0);
+    size_t length = BLOCK_SIZE;
 
     if (file.is_open()) {
         char* data = (char*) file.const_data();
@@ -54,7 +56,7 @@ int main() {
         char* eod = data + file_size - 1;
         int i = 0;
         size_t position = 0;
-        length = std::min(BLOCK, file_size - position);
+        length = std::min(BLOCK_SIZE, file_size - position);
         char* bob = bod;
         char* eob = bob + length - 1;
         while (bob && eob) {
@@ -64,7 +66,7 @@ int main() {
             if (!eob) {
                 break;
             }
-            threads.push_back(std::thread(d_count, i, bob, eob, results, lines, bytes));
+            threads.push_back(std::thread(d_count, i, bob, eob, std::ref(results), std::ref(lines), std::ref(bytes)));
             position += eob - bob + 1;
             if (position >= file_size) {
                 break;
@@ -73,7 +75,7 @@ int main() {
             if (!bob) {
                 break;
             }
-            length = std::min(BLOCK, file_size - position);
+            length = std::min(BLOCK_SIZE, file_size - position);
             eob = bob + length - 1;
             if (!eob) {
                 break;
@@ -88,7 +90,10 @@ int main() {
         int total_bytes = 0;
         int total_d = 0;
         int total_lines = 0;
-        for (int i = 0; i < THREADS; ++i) {
+        for (int i = 0; i < max_blocks; ++i) {
+            if (bytes[i] == 0) {
+                break;
+            }
             std::cout << "Thread: " << i;
             std::cout << ", Bytes: " << bytes[i];
             std::cout << ", Result: " << results[i];
