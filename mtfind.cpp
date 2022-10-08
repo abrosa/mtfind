1d34aecbbab340c93bbf5576abe5b311b80ee99e
Alexander Abrosov <aabrosov@gmail.com>
2022-10-08 20:54:22 +0300

    some optimizations + de'morgan rule

--- mtfind.cpp
+++ mtfind.cpp
@@ -9,11 +9,11 @@
 #include <chrono>
 #include <ctime>
 
-static const uintmax_t MAX_THREADS = 8;
+static const uint64_t MAX_THREADS = 7;
 
 class Block {
 public:
-    Block(uintmax_t n, uintmax_t l, char* begin, uintmax_t size, char* mask, uintmax_t mask_size, uintmax_t total_lines) {
+    Block(uint64_t n, uint64_t l, char* begin, uint64_t size, char* mask, uint64_t mask_size, uint64_t total_lines) {
         this->n = n;
         this->l = l;
         this->begin = begin;
@@ -22,42 +22,43 @@ public:
         this->mask_size = mask_size;
         this->total_lines = total_lines;
     }
-    uintmax_t n;  // number of block
-    uintmax_t l;  // number of line in block
+    uint64_t n;  // number of block
+    uint64_t l;  // number of line in block
     char* begin;
-    uintmax_t size;
+    uint64_t size;
     char* mask;
-    uintmax_t mask_size;
-    uintmax_t total_lines;
+    uint64_t mask_size;
+    uint64_t total_lines;
 };
 
 class Result {
 public:
-    Result(uintmax_t block, uintmax_t line, uintmax_t position, std::string found) {
+    Result(uint64_t block, uint64_t line, uint64_t position, std::string found) {
         this->block = block;
         this->line = line;
         this->position = position;
         this->found = found;
     }
-    uintmax_t block;
-    uintmax_t line;
-    uintmax_t position;
+    uint64_t block;
+    uint64_t line;
+    uint64_t position;
     std::string found;
 };
 
-inline bool compare_mask(char* line, char* mask, uintmax_t mask_size) {
-    uintmax_t i = 0;
-    for (; line && mask && i != mask_size; ++line, ++mask, ++i) {
-        if (*line == '\n' || *mask != '?' && *mask != *line) return false;
+bool compare_mask(char* line, char* mask, uint64_t mask_size) {
+    while (line && *line != '\n' && (*mask == '?' || *mask == *line)) {
+        ++line;
+        ++mask;
+        --mask_size;
+        if (mask_size == 0) return true;
     }
-    if (i == mask_size) return true;
     return false;
 }
 
 void process_line(Block & line, std::vector <Result> & results) {
     char* begin = line.begin;
-    uintmax_t len = line.size;
-    while (line.begin && len + begin - line.begin >= line.mask_size) {
+    uint64_t len = line.size;
+    while (line.begin && len + begin >= line.mask_size + line.begin) {
         if (compare_mask(line.begin, line.mask, line.mask_size)) {
             std::string found(line.begin, line.mask_size);
             Result current(line.n, line.l, 1l + line.begin - begin, found);
@@ -71,7 +72,7 @@ void process_block(Block & block, std::vector <Result> & results) {
     Result empty(block.n, 0, 0, "");
     results.push_back(empty);
     char* begin = block.begin;
-    uintmax_t len = block.size;
+    uint64_t len = block.size;
     while (block.begin && len > 0) {
         if (*block.begin == '\n') {
             Block curr(block.n, block.total_lines, begin, block.begin - begin + 1, block.mask, block.mask_size, 1);
@@ -85,7 +86,7 @@ void process_block(Block & block, std::vector <Result> & results) {
 }
 
 void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
-    uintmax_t total_lines = 1;
+    uint64_t total_lines = 1;
     for (auto& block : blocks) {
         for (auto& result : results) {
             if (result.block == block.n) {
@@ -94,7 +95,7 @@ void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
         }
         total_lines += block.total_lines;
     }
-    uintmax_t total_found = 0;
+    uint64_t total_found = 0;
     for (auto& result : results) {
         if (result.position != 0) {
             ++total_found;
@@ -112,8 +113,8 @@ void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
 
 void split_to_blocks(Block& block, std::vector<Block>& blocks)
 {
-    uintmax_t leftover = block.size;
-    uintmax_t block_size = leftover / MAX_THREADS;
+    uint64_t leftover = block.size;
+    uint64_t block_size = leftover / MAX_THREADS;
     while (block.begin && block.size > 0) {
         ++block.n;
         if (block.n > MAX_THREADS) {
@@ -140,7 +141,6 @@ void split_to_blocks(Block& block, std::vector<Block>& blocks)
 int main(int argc, char* argv[]) {
     auto start_time = std::chrono::system_clock::now();
     std::cout << "process started at " << start_time << std::endl;
-
     std::string file_name;
     std::string search_mask;
     if (argc < 3) {
@@ -151,27 +151,22 @@ int main(int argc, char* argv[]) {
         file_name = argv[1];
         search_mask = argv[2];
     }
-    // convert mask to pointer
-    uintmax_t mask_size = search_mask.length();
+    uint64_t mask_size = search_mask.length();
     char* mask = new char[mask_size + 1];
     strcpy_s(mask, mask_size + 1, search_mask.c_str());
-
     boost::iostreams::mapped_file file;
     file.open(file_name, boost::iostreams::mapped_file::mapmode::readwrite);
     if (file.is_open()) {
-        // std::cout << "block (" << 0 << ") line (" << 0 << ")" << std::endl;
         Block block(0, 0, (char*)file.const_data(), file.size(), mask, mask_size, 0);
         std::vector<Block> blocks;
         split_to_blocks(block, blocks);
         std::vector <std::thread> threads;
         std::vector <Result> results;
-        // without multithreading = 21s
-        // for (auto& block : blocks) {
-        //     process_block(block, results);
-        // }
-        // with multithreading = 7s
+        //for (auto& block : blocks) {
+        //    process_block(block, results);
+        //}
         for (auto& block : blocks) {
-            threads.push_back(std::thread(process_block, std::ref(block), std::ref(results)));
+           threads.push_back(std::thread(process_block, std::ref(block), std::ref(results)));
         }
         for (auto& thread : threads) {
             thread.join();
