53d09f4661f534f4ca4b69dcca5bd5e6be0cd68a
Alexander Abrosov <aabrosov@gmail.com>
2022-10-08 19:14:43 +0300

    removed excess counters from functions

--- mtfind.cpp
+++ mtfind.cpp
@@ -9,11 +9,11 @@
 #include <chrono>
 #include <ctime>
 
-static const size_t MAX_THREADS = 8;
+static const uintmax_t MAX_THREADS = 8;
 
 class Block {
 public:
-    Block(int n, int l, char* begin, size_t size, char* mask, size_t mask_size, int total_lines) {
+    Block(uintmax_t n, uintmax_t l, char* begin, uintmax_t size, char* mask, uintmax_t mask_size, uintmax_t total_lines) {
         this->n = n;
         this->l = l;
         this->begin = begin;
@@ -22,81 +22,70 @@ public:
         this->mask_size = mask_size;
         this->total_lines = total_lines;
     }
-    int n;  // number of block
-    int l;  // number of line in block
+    uintmax_t n;  // number of block
+    uintmax_t l;  // number of line in block
     char* begin;
-    size_t size;
+    uintmax_t size;
     char* mask;
-    size_t mask_size;
-    int total_lines;
+    uintmax_t mask_size;
+    uintmax_t total_lines;
 };
 
 class Result {
 public:
-    Result(int block, int line, int position, std::string found) {
+    Result(uintmax_t block, uintmax_t line, uintmax_t position, std::string found) {
         this->block = block;
         this->line = line;
         this->position = position;
         this->found = found;
     }
-    int block;
-    int line;
-    int position;
+    uintmax_t block;
+    uintmax_t line;
+    uintmax_t position;
     std::string found;
 };
 
-bool compare_mask(char* line, char* mask, size_t mask_size) {
-    while (*line != '\n' && mask_size > 0) {
-        if (*mask != '?' && *mask != *line) {
-            break;
-        }
-        ++line;
-        ++mask;
-        --mask_size;
+inline bool compare_mask(char* line, char* mask, uintmax_t mask_size) {
+    uintmax_t i = 0;
+    for (; line && mask && i != mask_size; ++line, ++mask, ++i) {
+        if (*line == '\n' || *mask != '?' && *mask != *line) return false;
     }
-    if (mask_size == 0) return true;
+    if (i == mask_size) return true;
     return false;
 }
 
 void process_line(Block & line, std::vector <Result> & results) {
-    // std::cout << "line (" << line.l << ")" << std::endl;
-    int curr_pos = 1;
-    while (line.begin && line.size >= line.mask_size) {
+    char* begin = line.begin;
+    uintmax_t len = line.size;
+    while (line.begin && len + begin - line.begin >= line.mask_size) {
         if (compare_mask(line.begin, line.mask, line.mask_size)) {
             std::string found(line.begin, line.mask_size);
-            Result current(line.n, line.l, curr_pos, found);
+            Result current(line.n, line.l, 1l + line.begin - begin, found);
             results.push_back(current);
         }
         ++line.begin;
-        --line.size;
-        ++curr_pos;
     }
 }
 
 void process_block(Block & block, std::vector <Result> & results) {
     Result empty(block.n, 0, 0, "");
     results.push_back(empty);
-    int curr_line = 0;
-    char* curr_begin = block.begin;
-    size_t curr_pos = 1;
-    while (block.begin && block.size > 0) {
+    char* begin = block.begin;
+    uintmax_t len = block.size;
+    while (block.begin && len > 0) {
         if (*block.begin == '\n') {
-            Block curr(block.n, curr_line, curr_begin, curr_pos, block.mask, block.mask_size, 1);
+            Block curr(block.n, block.total_lines, begin, block.begin - begin + 1, block.mask, block.mask_size, 1);
             process_line(curr, results);
-            curr_line++;
-            curr_begin = block.begin + 1;
-            curr_pos = 0;
+            len -= block.begin + 1 - begin;
+            begin = block.begin + 1;
             ++block.total_lines;
         }
         ++block.begin;
-        --block.size;
-        ++curr_pos;
     }
-    // std::cout << "    block (" << block.n << ") total (" << block.total_lines << ")" << std::endl;
 }
 
 void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
-    int total_lines = 1;
+    uintmax_t total_lines = 1;
     for (auto& block : blocks) {
         for (auto& result : results) {
             if (result.block == block.n) {
@@ -105,7 +94,7 @@ void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
         }
         total_lines += block.total_lines;
     }
-    int total_found = 0;
+    uintmax_t total_found = 0;
     for (auto& result : results) {
         if (result.position != 0) {
             ++total_found;
@@ -123,12 +112,11 @@ void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
 
 void split_to_blocks(Block& block, std::vector<Block>& blocks)
 {
-    size_t leftover = block.size;
-    size_t max_blocks = MAX_THREADS;
-    size_t block_size = leftover / MAX_THREADS;
+    uintmax_t leftover = block.size;
+    uintmax_t block_size = leftover / MAX_THREADS;
     while (block.begin && block.size > 0) {
         ++block.n;
-        if (block.n > max_blocks) {
+        if (block.n > MAX_THREADS) {
             break;
         }
         block.size = std::min(block_size, leftover);
@@ -151,7 +139,7 @@ void split_to_blocks(Block& block, std::vector<Block>& blocks)
 
 int main(int argc, char* argv[]) {
     auto start_time = std::chrono::system_clock::now();
-    //std::cout << "process started at " << start_time << std::endl;
+    std::cout << "process started at " << start_time << std::endl;
 
     std::string file_name;
     std::string search_mask;
@@ -164,7 +152,7 @@ int main(int argc, char* argv[]) {
         search_mask = argv[2];
     }
     // convert mask to pointer
-    size_t mask_size = search_mask.length();
+    uintmax_t mask_size = search_mask.length();
     char* mask = new char[mask_size + 1];
     strcpy_s(mask, mask_size + 1, search_mask.c_str());
 
@@ -198,8 +186,8 @@ int main(int argc, char* argv[]) {
     delete[] mask;
 
     auto end_time = std::chrono::system_clock::now();
-    //std::cout << "process finished at " << end_time << std::endl;
+    std::cout << "process finished at " << end_time << std::endl;
 
     std::chrono::duration<double> elapsed_seconds = end_time - start_time;
-    //std::cout << "elapsed seconds: " << elapsed_seconds.count() << "s" << std::endl;
+    std::cout << "elapsed seconds: " << elapsed_seconds.count() << "s" << std::endl;
 }
