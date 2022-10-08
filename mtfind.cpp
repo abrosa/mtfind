e17671470cf9ab74b06ec7aef7a977deafe8506e
Alexander Abrosov <aabrosov@gmail.com>
2022-10-08 11:36:01 +0300

    fixed merging results, processing line moved from processing block

--- mtfind.cpp
+++ mtfind.cpp
@@ -8,48 +8,38 @@
 #include <array>
 
 // static const size_t MAX_THREADS = 8;
-static const size_t BLOCK_SIZE = 65536;
+static const size_t BLOCK_SIZE = 16;
 
 // Ulysses.txt ~ 23,6 blocks * 65536
 
 class Block {
 public:
-    Block(int n, char* begin, size_t size, std::string mask) {
+    Block(int n, char* begin, size_t size, std::string mask, int total_lines) {
         this->n = n;
         this->begin = begin;
         this->size = size;
         this->mask = mask;
+        this->total_lines = total_lines;
     }
     int n;
     char* begin;
     size_t size;
     std::string mask;
+    int total_lines;
 };
 
 class Result {
 public:
-    Result(int line, int position, std::string found) {
+    Result(int block, int line, int position, std::string found) {
+        this->block = block;
         this->line = line;
         this->position = position;
         this->found = found;
     }
+    int block;
     int line;
     int position;
     std::string found;
-};
-
-class BlockResults {
-public:
-    BlockResults(int total_lines, size_t total_bytes, int total_found, std::vector<Result> results) {
-        this->total_lines = total_lines;
-        this->total_bytes = total_bytes;
-        this->total_found = total_found;
-        this->results = results;
-    }
-    int total_lines = 0;
-    size_t total_bytes = 0;
-    int total_found = 0;
-    std::vector <Result> results;
 };
 
 bool compare_mask(char* line, std::string mask) {
@@ -66,54 +56,62 @@ bool compare_mask(char* line, std::string mask) {
     return false;
 }
 
-void d_count(Block block, std::vector <BlockResults> & results) {
-    Result empty(0, 0, "");
-    BlockResults res(1, block.size, 0, {empty});
-    int position = 1;
-    while (block.begin && block.size > 0) {
-        if (*block.begin == '\n') {
-            ++res.total_lines;
-            position = 0;
+void process_line(Block& line, int curr_line, std::vector <Result> & results)
+{
+    int curr_pos = 1;
+    while (line.begin && line.size > 0) {
+        if (compare_mask(line.begin, line.mask)) {
+            std::string found(line.begin, line.mask.size());
+            Result current(line.n, curr_line, curr_pos, found);
+            results.push_back(current);
         }
-        if (compare_mask(block.begin, block.mask)) {
-            ++res.total_found;
-            std::string found(block.begin, block.mask.size());
-            Result current(res.total_lines, position, found);
-            res.results.push_back(current);
-        }
-        ++block.begin;
-        --block.size;
-        ++position;
-    }
-    int i = 0;
-    for (auto & result : results) {
-        if (i == block.n) {
-            result = res;
-        }
-        ++i;
+        ++line.begin;
+        --line.size;
+        ++curr_pos;
     }
 }
 
-void merge_results(std::vector <BlockResults>& results) {
+void process_block(Block block, std::vector <Result> & results) {
+    int curr_line = 1;
+    char* curr_begin = block.begin;
+    size_t curr_pos = 1;
+    while (block.begin && block.size > 0) {
+        if (*block.begin == '\n') {
+            Block curr(block.n, curr_begin, curr_pos, block.mask, 1);
+            process_line(curr, curr_line, results);
+            curr_line++;
+            curr_begin = block.begin + 1;
+            curr_pos = 0;
+            ++block.total_lines;
+        }
+        ++block.begin;
+        --block.size;
+        ++curr_pos;
+    }
+}
+
+void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
+    int total_lines = 0;
+    for (auto& block : blocks) {
+        for (auto& result : results) {
+            if (result.block == block.n) {
+                result.line += total_lines;
+            }
+        }
+        total_lines += block.total_lines;
+    }
     int total_found = 0;
     for (auto& result : results) {
-        total_found += result.total_found;
+        ++total_found;
     }
     std::cout << total_found << std::endl;
 }
 
-void print_results(std::vector <BlockResults>& results) {
-    int total_lines = -1;
-    int total_found = 0;
+void print_results(std::vector <Result> & results) {
     for (auto& result : results) {
-        for (auto& res : result.results) {
-            if (res.position != 0) {
-                std::cout << total_lines + res.line + 1;
-                std::cout << " " << res.position;
-                std::cout << " " << res.found << std::endl;
-            }
-        }
-        total_lines += result.total_lines - 1;
+        std::cout << result.line << " ";
+        std::cout << result.position << " ";
+        std::cout << result.found << std::endl;
     }
 }
 
@@ -124,7 +122,6 @@ void split_to_blocks(Block& block, std::vector<Block>& blocks)
     while (block.begin && block.size > 0) {
         ++block.n;
         if (block.n > max_blocks) {
-            std::cout << "block.n > max_blocks" << std::endl;
             break;
         }
         block.size = std::min(BLOCK_SIZE, leftover);
@@ -140,7 +137,6 @@ void split_to_blocks(Block& block, std::vector<Block>& blocks)
         blocks.push_back(block);
         block.begin += block.size;
         if (!block.begin) {
-            std::cout << "!block.begin" << std::endl;
             break;
         }
     }
@@ -161,20 +157,18 @@ int main(int argc, char* argv[]) {
     boost::iostreams::mapped_file file;
     file.open(file_name, boost::iostreams::mapped_file::mapmode::readwrite);
     if (file.is_open()) {
-        Block block(-1, (char*)file.const_data(), file.size(), search_mask);
+        Block block(-1, (char*)file.const_data(), file.size(), search_mask, 1);
         std::vector<Block> blocks;
         split_to_blocks(block, blocks);
-        std::vector<std::thread> threads;
-        Result empty(0, 0, "");
-        BlockResults emptys(0, 0, 0, {empty});
-        std::vector <BlockResults> results(blocks.size(), emptys);
+        std::vector <std::thread> threads;
+        std::vector <Result> results;
         for (auto& block : blocks) {
-            threads.push_back(std::thread(d_count, block, std::ref(results)));
+            threads.push_back(std::thread(process_block, block, std::ref(results)));
         }
         for (auto& thread : threads) {
             thread.join();
         }
-        merge_results(results);
+        merge_results(blocks, results);
         print_results(results);
         file.close();
     }
