6e303c76f84c1f2cab76adc559ecfc4a9d8016db
Alexander Abrosov <aabrosov@gmail.com>
2022-10-12 10:00:03 +0300

    fixes with eol symbols and line numbering

--- mtfind.cpp
+++ mtfind.cpp
@@ -45,14 +45,15 @@ public:
 };
 
 void process_block(Block & block, std::vector <Result> & results) {
+    //std::cout << "[" << block.number << "'" << block.end - block.begin + 1 << "]" << std::endl;
     char* i = block.begin;
     char* j;
     uint64_t k;
-    for (char* block_begin = block.begin; block_begin < block.end; ++block_begin) {
+    for (char* block_begin = block.begin; block_begin <= block.end; ++block_begin) {
         if (*block_begin != '\n') {
             continue;
         }
-        for (j = i; j < block_begin - mask_len; ++j) {
+        for (j = i; j < block_begin - 1; ++j) {
             for (k = 0; k < mask_len; ++k) {
                 if (!(j + k) || *(j + k) == '\n') break;
                 if (!mask_str[k] || mask_str[k] == '\0') break;
@@ -74,7 +75,7 @@ void process_block(Block & block, std::vector <Result> & results) {
 }
 
 void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
-    uint64_t total_lines = 0;
+    uint64_t total_lines = 1;
     for (auto& block : blocks) {
         for (auto& result : results) {
             if (result.block == block.number) {
@@ -101,27 +102,25 @@ void merge_results(std::vector <Block>& blocks, std::vector <Result>& results) {
 
 void split_to_blocks(char* file_begin, uint64_t file_size, std::vector<Block>& blocks) {
     char* block_begin = file_begin;
+    uint64_t block_size;
     char* block_end;
     uint64_t block_number = 0;
-    uint64_t block_lines = 1;
-    uint64_t block_size = file_size / MAX_THREADS;
+    uint64_t block_lines = 0;
+    uint64_t avg_block_size = file_size / MAX_THREADS;
     while (block_begin && block_size > 0) {
-        block_size = std::min(block_size, file_size);
+        block_size = std::min(avg_block_size, file_size);
         block_end = block_begin + block_size - 1;
-        if (!block_end) {
-            break;
-        }
-        while (block_end && *block_end != '\n') {
-            ++block_end;
+        while (block_size != 0 && block_end && *block_end != '\n') {
             ++block_size;
+            ++block_end;
         }
         Block new_block(block_number, block_lines, block_begin, block_end);
         blocks.push_back(new_block);
-        block_begin += block_size;
-        file_size -= block_size;
+        block_begin = block_end + 1;
         if (!block_begin) {
             break;
         }
+        file_size -= block_size;
         ++block_number;
         if (block_number >= MAX_THREADS) {
             break;
@@ -130,22 +129,25 @@ void split_to_blocks(char* file_begin, uint64_t file_size, std::vector<Block>& b
 }
 
 int main(int argc, char* argv[]) {
-    auto start_time = std::chrono::system_clock::now();
-    std::cout << "process started at " << start_time << std::endl;
+    auto now1 = std::chrono::system_clock::now();
+    auto time1 = std::chrono::system_clock::to_time_t(now1);
+    //std::cout << ctime(&time1) << std::endl;
     std::string file_name;
     std::string search_mask;
     if (argc < 3) {
-        file_name = "./resources/hugetext.bin";
-        search_mask = "n?gger";
+        //file_name = "../mtfind/resources/hugetext.bin";
+        //search_mask = "n?gger";
+        //file_name = "../mtfind/resources/Ulysses.txt";
+        //search_mask = "n?t?i?g";
+        file_name = "../mtfind/resources/dabadee.txt";
+        search_mask = "??ue";
     }
     else {
         file_name = argv[1];
         search_mask = argv[2];
     }
-
     mask_len = search_mask.length();
     strcpy_s(mask_str, mask_len + 1, search_mask.c_str());
-
     boost::iostreams::mapped_file file;
     file.open(file_name, boost::iostreams::mapped_file::mapmode::readwrite);
     if (file.is_open()) {
@@ -165,10 +167,10 @@ int main(int argc, char* argv[]) {
     else {
         std::cout << "could not map the file " << file_name << std::endl;
     }
-
-    auto end_time = std::chrono::system_clock::now();
-    std::cout << "process finished at " << end_time << std::endl;
-
-    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
-    std::cout << "elapsed seconds: " << elapsed_seconds.count() << "s" << std::endl;
+    auto now2 = std::chrono::system_clock::now();
+    auto time2 = std::chrono::system_clock::to_time_t(now2);
+    //std::cout << ctime(&time2) << std::endl;
+    std::chrono::duration<double> elapsed_seconds = now2 - now1;
+    std::cout << "multi-threads: " << elapsed_seconds.count() << "s" << std::endl;
+    return 0;
 }
